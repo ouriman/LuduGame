d5871243c995f372a70a22247927fdff1baa0c3a
Belayet Hossain <117828783+ouriman@users.noreply.github.com>
2022-12-03 11:51:28 +0600

    Ludu game development

---
+++ LuduGame.py
@@ -0,0 +1,9 @@
+import random
+
+ludu= [1,2,3,4,5,6]
+random_number= random.choice(ludu)
+
+if random_number ==6:
+    print(random_number, 'You own Chhokka!')
+else:
+    print(random_number, 'is not Chhokka!')
